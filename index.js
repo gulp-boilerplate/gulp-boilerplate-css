ba396459e3308023c2829c87bb6def301eb55a6e
Marcos Hernández <marcoslhc@gmail.com>
2015-04-06 14:27:15 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    csso = require('gulp-csso'),
+    gutil = require('gulp-util'),
+    uncss = require('gulp-uncss'),
+    concat = require('gulp-concat'),
+    print = require('gulp-print'),
+    symbols = require('symbols'),
+    success = symbols.success,
+    error = symbols.error,
+    autoprefixer = require('gulp-autoprefixer');
+
+module.exports = function (config) {
+    return function () {
+        return gulp.src(config.src)
+            .on('error', gutil.log.bind(this, error + ' CSS Error '))
+            .pipe(less())
+            .pipe(print(function (filepath) {
+                return gutil.colors.green(success) + ' ' + filepath;
+            }))
+            .pipe(concat('site.css'))
+            .pipe(uncss({
+                html: ['index.html']
+            }))
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(csso())
+            .pipe(gulp.dest(config.dest));
+    };
+};
