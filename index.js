a90050dfa2866a817c348388b15ae79fb88c604b
Marcos Hernández <marcoslhc@gmail.com>
2015-04-10 11:12:04 -0400

    Add cache

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     print = require('gulp-print'),
     symbols = require('symbols'),
+    remember = require('remember'),
+    cached = require('gulp-cached'),
     success = symbols.success,
     error = symbols.error,
     autoprefixer = require('gulp-autoprefixer');
@@ -13,11 +15,13 @@ var gulp = require('gulp'),
 module.exports = function (config) {
     return function () {
         return gulp.src(config.src)
+            .pipe(cached('styles'))
             .on('error', gutil.log.bind(this, error + ' CSS Error '))
             .pipe(less())
             .pipe(print(function (filepath) {
                 return gutil.colors.green(success) + ' ' + filepath;
             }))
+            .pipe(remember('styles'))
             .pipe(concat('site.css'))
             .pipe(uncss({
                 html: ['index.html']
