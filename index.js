ae0fb5b9dd175c0cd1e7d1b5a297ded714cc566a
Marcos Hernández <marcoslhc@gmail.com>
2015-09-29 17:20:09 -0400

    Use lazypipe and gulp-if to set conditionals

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var gulp = require('gulp'),
     symbols = require('symbols'),
     remember = require('gulp-remember'),
     cached = require('gulp-cached'),
+    lazypipe = require('lazypipe'),
+    gulpif = require('gulp-if'),
     success = symbols.success,
     error = symbols.error,
     autoprefixer = require('gulp-autoprefixer'),
@@ -30,32 +32,27 @@ module.exports = function (config) {
 
         var stream;
 
-        stream = gulp.src(config.src)
-            .pipe(cached('styles'))
-            .on('error', gutil.log.bind(this, error + ' CSS Error '));
+        stream = lazypipe()
+        .pipe(cached('styles'))
+            .on('error', gutil.log.bind(this, error + ' CSS Error '))
+        .pipe(gulpif(precompiler, precompiler()))
+        .pipe(print(function (filepath) {
+            return gutil.colors.green(success) + ' ' + filepath;
+        }))
+        .pipe(remember('styles'))
+        .pipe(concat('site.css'))
+        .pipe(gulpif(config.options.uncss, uncss({
+            html: config.options.uncss.files || ['index.html']
+        })))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(csso());
 
-        if (precompiler) {
-            stream.pipe(precompiler());
-        }
-
-        stream.pipe(print(function (filepath) {
-                return gutil.colors.green(success) + ' ' + filepath;
-            }))
-            .pipe(remember('styles'))
-            .pipe(concat('site.css'));
-
-        if (config.options.uncss) {
-            stream.pipe(uncss({
-                html: config.options.uncss.files || ['index.html']
-            }));
-        }
-
-        // stream.pipe(autoprefixer({
-        //     browsers: ['last 2 versions'],
-        //     cascade: false
-        // }))
-        stream.pipe(csso())
-            .pipe(gulp.dest(config.dest));
+        gulp.src(config.src)
+        .pipe(stream())
+        .pipe(gulp.dest(config.dest));
         return gulp;
     };
 };
